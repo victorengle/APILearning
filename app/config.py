ae6fa1772cc99eba99784b49784a3b3b71ee087b
Victor Engledal <victor.engledal@semine.no>
2022-06-03 14:46:18 +0200

    dependencies and install python added env variables

--- app/config.py
+++ app/config.py
@@ -5,11 +5,11 @@ class Settings(BaseSettings):
     Database_Hostname: str
     Database_Name: str
     Database_Username: str
-    Database_Port: int
+    Database_Port: str
     Database_Password: str
     Secret_Key: str
     Algorithm: str
-    Access_Token_Expire_Minutes: str
+    Access_Token_Expire_Minutes: int
 
     class Config:
         env_file = ".env"
