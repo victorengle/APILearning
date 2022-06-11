ac0afb999983a394c8e11245f0ba6edf8cdb7b32
victorengle <ify_dunga@yahoo.com>
2022-06-11 19:10:21 +0200

    testing ssh deployment 3

--- app/main.py
+++ app/main.py
@@ -35,4 +35,4 @@ app.include_router(votes.router)
 @app.get("/")
 def gethello():
     print("Yolo")
-    return {"Message": "Seeing this message means CI/CD Is Completed"}
+    return {"Message": "Seeing this message means CI/CD Is Completed! Hurray!"}
